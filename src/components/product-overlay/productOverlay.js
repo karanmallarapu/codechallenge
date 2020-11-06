9d929e1364b5177bac5d53d7cd0bb17c3817b747
karanmallarapu <33434782+karanmallarapu@users.noreply.github.com>
2020-11-06 10:43:53 -0700

    Update productOverlay.js

--- src/components/product-overlay/productOverlay.js
+++ src/components/product-overlay/productOverlay.js
@@ -2,33 +2,52 @@ import React, { Component } from 'react';
 import data from '../data/products.json';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import './productOverlay.css';
 
 class ProductOverlay extends Component {
     state = {
         loadedProduct: null,
-        productImages: []
+        productImages: [],
+        isDisplayed:false
     }
-    componentDidUpdate() {
-        if (this.props.id) {
-            if (!this.state.loadedProduct || (this.state.loadedProduct && this.state.loadedProduct.id !== this.props.id)) {
-                let product = data.groups.find(product => {
-                    return product.id = this.props.id;
-                });
-                this.setState({ loadedProduct: product })
-                this.setState({ productImages: product.images })
+    componentDidUpdate(prevProps,nextProps) {
+        if(prevProps!=this.props)
+        {
+            if (this.props.id) {
+                if (!this.state.loadedProduct || (this.state.loadedProduct && this.state.loadedProduct.id !== this.props.id)) {
+                    let product = data.groups.find(product => {
+                        return product.id === this.props.id;
+                    });
+                    this.setState({
+                        loadedProduct: product,
+                        productImages: product.images,
+                        isDisplayed:this.props.displayed
+                    });
+                }
             }
         }
+        
+    }
+    removeOverLayHandler=()=>{
+        this.setState({isDisplayed:!this.state.isDisplayed})
     }
     render() {
-        this.state.productImages.map(image=>{
-            console.log(image.href);
-        })
-        return(
-            <div>
-                
+        const images = this.state.productImages.map((image, index) => {
+            return <div key={index} onClick={()=>this.removeOverLayHandler()}>
+                <img src={image.href} alt={image.href}/>
+            </div>
+        });
+        return (
+            <div className="overlay" 
+            style={{display:this.state.isDisplayed?'block':'none'}} >
+                <div className="carousel">
+                    <Carousel>
+                        {images}
+                    </Carousel>
+                </div>
             </div>
         )
-        
+
     }
 }
 export default ProductOverlay;
